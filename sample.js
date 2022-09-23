af97e985c0c9cb85ce3e13c22019cec249676d85
johnpathadan <johnpathadan@gmail.com>
2022-09-23 16:24:59 +0530

    fix: array sample

---
+++ sample.js
@@ -0,0 +1,79 @@
+const selectAnganwadi = [
+    {
+      id: 1,
+      value: 'anganwadi-1',
+      label: 'Anganwadi 1',
+    },
+    {
+      id: 2,
+      value: 'anganwadi-2',
+      label: 'Anganwadi 2',
+    },
+    {
+      id: 3,
+      value: 'anganwadi-3',
+      label: 'Anganwadi 3',
+    },
+    {
+      id: 4,
+      value: 'anganwadi-4',
+      label: 'Anganwadi 4',
+    },
+    {
+      id: 5,
+      value: 'anganwadi-5',
+      label: 'Anganwadi 5',
+    },
+    {
+      id: 6,
+      value: 'anganwadi-1',
+      label: 'Anganwadi 1',
+    },
+    {
+      id: 7,
+      value: 'anganwadi-2',
+      label: 'Anganwadi 2',
+    },
+    {
+      id: 8,
+      value: 'anganwadi-3',
+      label: 'Anganwadi 3',
+    },
+    {
+      id: 9,
+      value: 'anganwadi-4',
+      label: 'Anganwadi 4',
+    },
+    {
+      id: 10,
+      value: 'anganwadi-5',
+      label: 'Anganwadi 5',
+    },
+    {
+      id: 11,
+      value: 'anganwadi-1',
+      label: 'Anganwadi 1',
+    },
+    {
+      id: 12,
+      value: 'anganwadi-2',
+      label: 'Anganwadi 2',
+    },
+    {
+      id: 13,
+      value: 'anganwadi-3',
+      label: 'Anganwadi 3',
+    },
+    {
+      id: 14,
+      value: 'anganwadi-4',
+      label: 'Anganwadi 4',
+    },
+    {
+      id: 15,
+      value: 'anganwadi-5',
+      label: 'Anganwadi 5',
+    },
+  ];
+  
+  console.log(selectAnganwadi.length);
